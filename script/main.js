73777b6396fee665da6b7d8e9d182fd371088c3a
csslick <digitone@naver.com>
2022-07-21 16:39:17 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -21,9 +21,9 @@ $(function(){
   // slider
   let num = 0;  // 이미지 번호(전역변수)
 
-  function changeSlider(img_num) {
+  function changeSlider(a) {
     // 공식 = 높이값(세로기준) * 이미지 번호
-    let img_height = 300 * img_num;
+    let img_height = 300 * a;
 
     $('.slider .sliders').css({
       transform: `translateY(${-img_height}px)`
@@ -33,6 +33,7 @@ $(function(){
   // 자동재생
   setInterval(function(){
     num++;
+    // 마지막 번호이면 처음으로
     if(num > 2) { num = 0; }
     changeSlider(num);
   }, 3000);
